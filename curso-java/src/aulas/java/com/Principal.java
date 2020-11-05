83561b4220f8722d03cf301ac59ff73035b2fefb
Everaldo Nascimento <everaldobass@gmail.com>
2020-11-04 22:16:41 -0300

    Adicionei arquivos
    Adicionei arquivos no Git.

---
+++ curso-java/src/aulas/java/com/Principal.java
@@ -0,0 +1,11 @@
+package aulas.java.com;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aulas de Java no Git ");
+
+	}
+
+}
