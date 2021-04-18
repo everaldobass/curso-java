fc20d89cc017eb7add3b9a359d36751e17f581bd
Everaldo Nascimento <everaldobass@gmail.com>
2021-04-18 11:42:54 -0300

    App springboot

---
+++ workspace/AppSpring/src/main/java/com/tornese/java/appProject/AppProjectApplication.java
@@ -0,0 +1,14 @@
+package com.tornese.java.appProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProjectApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(AppProjectApplication.class, args);
+		System.out.println("Olá Mundo!");
+	}
+
+}
---
+++ workspace/AppSpring/src/test/java/com/tornese/java/appProject/AppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornese.java.appProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
